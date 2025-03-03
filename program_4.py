3c1e26a558d418aae4d27843787ab4d942368bd4
TFoster16 <northwesterntim@gmail.com>
2025-03-03 11:08:43 -0600

    program_4.py

--- program_4.py
+++ program_4.py
@@ -1,10 +1,53 @@
-# Program #4: Coordinates
-# Write a distance function that will take two 3-dimensional coordinates (as input) 
-# and will return (as output) the distance between those points in space.  
-# The 3-dimensional coordinates must be stored as tuples.
+#Timothy Foster, 3/3/25, Coordinates Program
 
-# Now write a mainline that has the user enter the two tuples.  
-# The mainline calls the distance function and stores the distance in a variable.  The mainline then displays the distance.  
-# Also include exception handling to deal with faulty input.
-# The distance between two points (x1,y1,z1) and (x2, y2, z2) is 
-#    given by:   sqrt ((x2-x1)^2 + (y2 - y1)^2 + (z1 - z2)^2) 
+#Define the calculation function.
+def calculate_distance(coordinates_1, coordinates_2):
+
+    #Import the math module.
+    import math
+
+    #Assign the values from the tuples to variables and convert into float values.
+    x1 = float(coordinates_1[0])
+    x2 = float(coordinates_2[0])
+    y1 = float(coordinates_1[1])
+    y2 = float(coordinates_2[1])
+    z1 = float(coordinates_1[2])
+    z2 = float(coordinates_2[2])
+
+    #Return the distance value using the formula.
+    return math.sqrt( ((x2 - x1) ** 2) + ((y2 - y1) ** 2) + ((z1 - z2) ** 2) )
+
+#Define the main function.
+def main():
+
+    #Get user input for the first set of coordinates.
+    coordinates_1 = (input("Enter the x-value for the first set of coordinates:"),
+                     input("Enter the y-value for the first set of coordinates:"),
+                     input("Enter the z-value for the first set of coordinates:"))
+
+    #Ensure all of the inputs are numbers.
+    if coordinates_1[0].isnumeric() == False or coordinates_1[1].isnumeric() == False or coordinates_1[2].isnumeric() == False:
+        print("You must input numerical values for the coordinates that do not contain decimal points.")
+
+        #Run the program again and quit before it can run into an error.
+        main()
+        quit()
+
+    coordinates_2 = (input("Enter the x-value for the second set of coordinates:"),
+                     input("Enter the y-value for the second set of coordinates:"),
+                     input("Enter the z-value for the second set of coordinates:"))
+
+    if coordinates_2[0].isnumeric() == False or coordinates_2[1].isnumeric() == False or coordinates_2[2].isnumeric() == False:
+        print("You must input numerical values for the coordinates that do not contain decimal points.")
+        main()
+        quit()
+
+    #Assign the distance variable using the above function.
+    distance = calculate_distance(coordinates_1, coordinates_2)
+
+    #Display the results.
+    print(f'The distance between the two sets of coordinates is {distance: .3f} units.')
+
+#Run the above function.
+if __name__ == "__main__":
+    main()
